cc99011ade8e9c4445eea4835e889f4a164b877e
eyurtseven <yurt7emre@gmail.com>
2015-06-11 23:04:25 +0300

    .

---
+++ ProductDemo/ProductDemo.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ProductDemo.Core.Infrastructure;
+using ProductDemo.Web.ViewModel;
+
+namespace ProductDemo.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var product = _productRepository.GetById(id.Value);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var allCategories = _categoryRepository.GetAll().ToList();
+
+            var pageModel = new ProductPageModel
+            {
+                CurrentProduct = product,
+                CategoryList = allCategories
+            };
+
+            return View(pageModel);
+        }
+    }
+}
---
+++ ProductDemo/ProductDemo.Web/ViewModel/ProductPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProductDemo.Data.Model;
+
+namespace ProductDemo.Web.ViewModel
+{
+    public class ProductPageModel
+    {
+        public Product CurrentProduct { get; set; }
+        public List<Category> CategoryList { get; set; }
+    }
+}
