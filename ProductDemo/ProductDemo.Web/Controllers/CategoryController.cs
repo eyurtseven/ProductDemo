cd66fc8af365ec6b5554c9c0f8af54cc70f94f87
eyurtseven <yurt7emre@gmail.com>
2015-06-11 17:27:33 +0300

    Category View eklendi

---
+++ ProductDemo/ProductDemo.Web/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ProductDemo.Core.Infrastructure;
+using ProductDemo.Web.ViewModel;
+
+namespace ProductDemo.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var allCategories = _categoryRepository.GetAll().ToList();
+
+            var category = allCategories.FirstOrDefault(x => x.CategoryId == id);
+
+            if (category == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var products = category.Products.ToList();
+
+            var pageModel = new CategoryPageModel()
+            {
+                CurrentCategory = category,
+                CategoryList = allCategories,
+                ProductList = products
+            };
+
+            return View(pageModel);
+        }
+    }
+}
---
+++ ProductDemo/ProductDemo.Web/ViewModel/CategoryPageModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ProductDemo.Data.Model;
+
+namespace ProductDemo.Web.ViewModel
+{
+    public class CategoryPageModel
+    {
+        public Category CurrentCategory { get; set; }
+        public List<Category> CategoryList { get; set; }
+        public List<Product> ProductList { get; set; }
+    }
+}
